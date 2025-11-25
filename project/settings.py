ccb65c638eea895a7230c9db58fd4aa90372d507
Carlos Celestino <49007553+carloscelestino1@users.noreply.github.com>
2025-11-25 10:03:09 -0300

    aa
    aa

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # indicação de onde vou manter meus html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
